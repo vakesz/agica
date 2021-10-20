8eca476e69ba7707fbd5f97d2ccd003e8c912d4d
Gabor Pinkova <gabor.pinkova@gmail.com>
2021-10-20 13:50:52 +0200

    Initial design

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    const burgerIcon = document.querySelector('#burger');
+    const navbarMenu = document.querySelector('#nav-links');
+    const menuItem = document.getElementById('#menu-item');
+
+    burgerIcon?.addEventListener('click', () => {
+      navbarMenu?.classList.toggle('is-active');
+      burgerIcon?.classList.toggle('is-active');
+    });
+
+    navbarMenu?.addEventListener('click', () => {
+      navbarMenu?.classList.toggle('is-active');
+      burgerIcon?.classList.toggle('is-active');
+    });
+  }
+
+}
