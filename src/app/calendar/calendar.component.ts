1abb881c32efecfcf01d8a4dd44614ebac19faa8
Gabor Pinkova <gabor.pinkova@gmail.com>
2021-10-20 18:56:59 +0200

    Add icons

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -16,7 +16,7 @@ export class CalendarComponent implements OnInit {
 
   ngOnInit(): void {  {
 
-    var calendarId = 'hu.hungarian#holiday@group.v.calendar.google.com'; // TO BE CHANGED
+    var calendarId = 'bgudccobvjdigft93bk4njsvt0@group.calendar.google.com'; // TO BE CHANGED
     var apiKey = 'AIzaSyCU-1fH-6AFoDLS4CpUf25NS_-EO_MqmEQ';
     var userTimeZone = "Europe/Budapest";
 
@@ -40,10 +40,14 @@ export class CalendarComponent implements OnInit {
                 var calendarRows = [];
                 console.log(response.result.items);
                 response.result.items.forEach(function(entry) {
-                    var startsAt = moment(entry.start.date).format("YYYY-MM-DD") + ' ' + moment(entry.start.dateTime).format("HH:mm");
+                    var startsAt = moment(entry.start.dateTime).format("YYYY MMMM DD.") + ' ' + moment(entry.start.dateTime).format("HH:mm");
                     var endsAt = moment(entry.end.dateTime).format("HH:mm");
                     calendarRows.push(`<div class="box"><article class="media"><div class="media-left"><figure class="image is-64x64">`);
-                    calendarRows.push(`<img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"></figure></div><div class="media-content">`);
+                    if (entry.location == "PUBG") calendarRows.push(`<img src="assets/img/pubg.webp" alt="Image">`);
+                    if (entry.location == "R6") calendarRows.push(`<img src="assets/img/r6.webp" alt="Image">`);
+                    if (entry.location == "VALORANT") calendarRows.push(`<img src="assets/img/valorant.webp" alt="Image">`);
+                    if (entry.location == "CHAT") calendarRows.push(`<img src="assets/img/chat.webp" alt="Image">`);
+                    calendarRows.push(`</figure></div><div class="media-content">`);
                     calendarRows.push(`<div class="content"><p><strong> ${entry.summary} </strong> <small>${startsAt} - ${endsAt}</small>`);
                     calendarRows.push(`<br> ${entry.description} </p></div></div></article></div>`);
                 });
