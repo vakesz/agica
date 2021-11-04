374c09bec4a450a44e74d04f91a2702660690e98
Gabor Pinkova <gabor.pinkova@gmail.com>
2021-11-04 18:29:02 +0100

    Fix video page

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -11,9 +11,23 @@ import * as moment from "moment";
 })
 export class VideosComponent implements OnInit {
   videos!: any;
+  targetModal! : any;
   private unsubscribe$: Subject<any> = new Subject();
 
   constructor(private googleService: GoogleService) { }
+
+  playVideo(event:any, video:any) {
+    let target = event.target.id;
+
+    this.targetModal = document.querySelector('#yt-modal');
+    (<HTMLIFrameElement>document.getElementById('iframe')).src = "https://www.youtube.com/embed/" + target;
+    this.targetModal?.classList.remove('hidden');
+  }
+
+  closeVideo(event:any) {
+    let targetCloseModal = document.querySelector('#modal-background');
+    this.targetModal?.classList.add('hidden');
+  }
 
   ngOnInit() {
     this.videos = [];
@@ -22,12 +36,15 @@ export class VideosComponent implements OnInit {
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe((data) => {
         this.videos = data;
+        this.formatDates();
+        console.log(data);
       });
     }
 
-  formatVideoId(): void {
+  formatDates(): void {
+    moment.locale('HU');
     for (let i in this.videos) {
-      this.videos[i].id.videoId = 0;
+      this.videos[i].snippet.publishTime = moment(this.videos[i].snippet.publishTime).format("YYYY MMMM DD");
     }
   }
 
